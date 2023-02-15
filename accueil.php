f61d86f8cbfbe969fc0d90f315007487f8da66c9
Robert KULE <kulewakangitsirobert@gmail.com>
2023-02-15 15:11:58 +0200

    ajout du fichier accueil.php

---
+++ accueil.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Valeurs tapés</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Valeur tapé</h1>
+    <?php echo'le nom tapé est : '.$_POST['nom'].'<br>
+    le mot de passe est '.$_POST['mdpass'].'<br>Gracias' ?>
+</body>
+</html>
